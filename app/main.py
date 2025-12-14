c206cc33f6cc8a647ff15af1240bb1c0cbbbaa10
Danylo Moroz <danylomoroz1989@gmail.com>
2025-12-14 18:55:47 +0100

    Solution_moroz_rounded_correct

--- app/main.py
+++ app/main.py
@@ -19,9 +19,12 @@ class CarWashStation:
         count = 0.0
         for car in cars:
             if car.clean_mark < self.clean_power:
-                count += self.calculate_washing_price(car)
+                count += (car.comfort_class
+                          * (self.clean_power - car.clean_mark)
+                          * self.average_rating
+                          / self.distance_from_city_center)
                 self.wash_single_car(car)
-        return count
+        return round(count, 1)
 
     def calculate_washing_price(self, car: Car) -> float:
         return round((car.comfort_class * (self.clean_power - car.clean_mark)
